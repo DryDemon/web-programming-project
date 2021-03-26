893b248ef232d0a8ef0dad7598990766d2020cc3
jean chartier-Kastler <jeancroco@gmail.com>
2021-03-26 14:55:52 +0100

    suppression des fichiers boosstrap studio

--- backend/jwtFunctions.php
+++ backend/jwtFunctions.php
@@ -4,6 +4,6 @@
     $publicJwtKey = "fHGQJ1BHTNB/r4JXzpvYiG5UL0yfI+S8Q8tJBKamEAE=";
 
     function getJwtKey(){
-        return $jwtKey;
+        return "pz4CpsaI3bZtucQB8B5jONE073DM33fd4eHyvE0a4pg";
     }
 ?>
