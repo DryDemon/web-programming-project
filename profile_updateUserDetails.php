fe4757f9c4af06356fb44aac6237cf0c981bc732
jean chartier-Kastler <jeancroco@gmail.com>
2021-03-30 22:51:22 +0200

    fonction update profile finis

--- profile_updateUserDetails.php
+++ profile_updateUserDetails.php
@@ -1,51 +1,65 @@
 <?php
-    function updateUsernameStuff(){
+require_once "mysqlConnection.php";
+require_once "userConnection.php";
 
-        $username = $_POST['username'];   
-        $last_name = $_POST['last_name'];   
-        $first_name = $_POST['first_name'];   
-        $email = $_POST['email'];   
+function updateUsernameStuff()
+{
 
+    $username = $_POST['username'];
+    $last_name = $_POST['last_name'];
+    $first_name = $_POST['first_name'];
+    $email = $_POST['email'];
 
-        
-    }
-
+    $id = getCurrentUserData()[0];
     
-    function updateAdressStuff(){
-        $adress = $_POST['adress'];   
-        $city = $_POST['city'];   
-        $country = $_POST['country'];   
+    $dbConn = MyConnection();
+    $update = $dbConn->query("Update User SET Nickname='" . $username . "', LastName='" . $last_name . "', Name='" . $first_name . "', Email='" . $email . "' WHERE id=" . $id);
+    echo("Update User SET Nickname='" . $username . "', LastName='" . $last_name . "', Name='" . $first_name . "', Email='" . $email . "' WHERE id=" . $id);
+}
 
+function updateAdressStuff()
+{
+    $adress = $_POST['adress'];
+    $city = $_POST['city'];
+    $country = $_POST['country'];
+    
+    $id = getCurrentUserData()[0];
+    
+    $dbConn = MyConnection();
+    $update = $dbConn->query("Update User SET Adress='" . $adress . "', City='" . $city . "', Country='" . $country . "' WHERE id=" . $id);
+    
+}
+function updateCardStuff()
+{
+    $ccn = $_POST['ccn'];
+    $exp = $_POST['exd'];
+    $cvc = $_POST['cvc'];
+    
+    $id = getCurrentUserData()[0];
+    
+    $dbConn = MyConnection();
+    $update = $dbConn->query("Update User SET ccnumber='" . $ccn . "', expdate='" . $exp . "', cvnumber='" . $cvc . "' WHERE id=" . $id);
+}
+
+if (isset($_POST['type'])) {
+    
+    $type = $_POST['type'];
+
+    switch ($type) {
+        case 0:
+            updateUsernameStuff();
+            break;
+        case 1:
+            updateAdressStuff();
+
+            break;
+        case 2:
+            updateCardStuff();
+            break;
     }
-    function updateCardStuff(){
-        $ccn = $_POST['ccn'];   
-        $exp = $_POST['exd'];   
-        $cvc = $_POST['cvc'];   
+}
 
-    }
+refreshCookie();
 
-
-    if(isset($_POST['type'])){
-
-        $type=$_POST['type'];
-         
-        switch($type){
-            case 0:
-                    updateUsernameStuff();
-                break;
-                case 1:
-                    updateAdressStuff();
-
-                break;
-            case 2:
-                updateCardStuff();
-                break;
-        }
-    }
-
-
-    //redirect
-    // header( "Location: "."http://".$_SERVER["HTTP_HOST"]."/profile.php");
-
-
-?>
+//redirect
+header( "Location: "."http://".$_SERVER["HTTP_HOST"]."/profile.php");
