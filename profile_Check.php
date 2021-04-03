51204129d7c33b8497bba3504424966a35b94fb0
Augustin-source <gusbert78@gmail.com>
2021-04-03 16:41:38 +0200

    test

--- profile_Check.php
+++ profile_Check.php
@@ -1,15 +1,37 @@
 <?php
 
-require_once "mysqlConnection.php";
-require_once "userConnection.php";
+require_once("mysqlConnection.php");
+$dbConn = MyConnection();
 
-$user = getCurrentUserData();//4,5,6,9,10,11
 
-if($user[4]!='vide' && $user[5]!='vide' && $user[6]!='vide' && $user[9]!=1 && $user[10]!='vide' && $user[11]!=0){
-    return true;//information completed
-}
-else{
-    return false;//missing information
-}
+    function analyseUser()
+    {
+        require_once "mysqlConnection.php";
+        require_once "userConnection.php";
 
+        $user = getCurrentUserData();
+
+        if($user[4]!='vide' && $user[5]!='vide' && $user[6]!='vide' && $user[9]!=1 && $user[10]!='vide' && $user[11]!=0){
+            return true;//information completed
+        }
+        else{
+            return false;//missing information
+        }
+    }
+
+    function getIdSeller($idproduct)
+    {
+        $resultId = $dbConn->query('SELECT idSeller FROM Transaction WHERE idproduct = '.$idproduct.);
+        return $resultId;
+    }
+
+    function createNegociation()
+    {
+        require_once "userConnection.php";
+        $iduser = getCurrentUserData();
+        $idproduct;
+        $idseller =  getIdSeller($idproduct);
+        $currentOffer;
+        $Offer = $_POST['Offer'];
+    }
 ?>
