f7b9510defd646fb0f85aa8f45d36ca2ebcf8dda
jean chartier-kastler <jean.chartierkastler@gmail.com>
2021-04-04 20:16:29 +0200

    modif product page, need to fill out date for auction

--- Product.php
+++ Product.php
@@ -5,13 +5,48 @@
     <meta charset="utf-8">
     <title>Product</title>
     <link rel="stylesheet" href="productStyle.css">
+    <script type="text/javascript">
+        function showDateEnd() {
+            var x = document.getElementById("dateEnd");
+            document.getElementById("dateEndInput").required = true;
+            x.style.display = "block";
+        }
+
+        function hideDateEnd() {
+            var x = document.getElementById("dateEnd");
+            document.getElementById("dateEndInput").required = false;
+            x.style.display = "none";
+        }
+        window.onload = function(e) {
+            hideDateEnd();
+        }
+
+        function onTypeChoice(e) {
+            switch (e.target.value) {
+                case "Auction":
+                    showDateEnd()
+                    break;
+                case "Best Offer":
+                    hideDateEnd()
+                    break;
+                case "Instant Buy":
+                    hideDateEnd()
+                    break;
+                default:
+                    hideDateEnd()
+                    break;
+
+            }
+        }
+    </script>
 </head>
 
 <body>
-    <div class="MainTitle"><h4>FUNMARKET</h4>
+    <div class="MainTitle">
+        <h4>FUNMARKET</h4>
         <button class="back" onclick="window.location.href='Shop.php'" style="color: white;">Back</button>
     </div>
-    
+
     <div class="container">
         <div class="card">
             <div class="cardBody">
@@ -23,43 +58,44 @@
                             </div>
                             <form method="POST" action="./ProductRegistering.php" class="product">
                                 <div class="PName">
-                                    <div ><input class="small" name="ProductName" type="text" placeholder="Product Name"></div>
-                                 </div>
-                                 <div class="PCategory" style="margin-left: 2%;">
-                                    <div ><input class="small" name="ProductCategory" list="browsers" placeholder="Choose the category">
-                                        <datalist id="browsers">
-                                          <option value="Computer">
-                                          <option value="Phone & SmartPhone">
-                                        </datalist>
-                                    </div>
-                                 </div>
-                                 <div class="PType" style="margin-left: 5%;">
-                                 <input class="small" name="ProductType" list="test" placeholder="Choose the type of the selling">
-                                        <datalist id="test">
-                                          <option value="Auction">
-                                          <option value="Best Offer">
-                                          <option value="Instant Buy">
-                                        </datalist>
+                                    <div><input class="small" name="ProductName" type="text" placeholder="Product Name" required></div>
                                 </div>
-                                 <div class="PDescription"><input class="large" name="ProductDescription" type="textarea" placeholder="Product Description" style="height: 200px;"></div>
-                                 <div class="PPrice">
-                                     <div class="Price" ><input class="small" name="ProductPrice" type="number" placeholder="Product Price ($)"></div>
-                                 </div>
-                                 <div class="PEd"><a style="margin-left: 33%;">Select a *DeadLine* if needed</a> <input class="small" name="ProductDeadLine" type="date" ></div>
-                                 <div class="PPhoto">
-                                    <div class="Photo" >
-                                        <a style="margin-left: 33%;">Add some pictures ?</a>
-                                        <input class="small" placeholder="http://google.com/image.png" name="ProductImage" type="url" ></div>
+                                <div class="PCategory" style="margin-left: 2%;">
+                                    <div><input class="small" name="ProductCategory" list="browsers" placeholder="Choose the category" required>
+                                        <datalist id="browsers">
+                                            <option value="Computer">
+                                            <option value="Phone & SmartPhone">
+                                        </datalist>
                                     </div>
-                                    <!-- <div class="Video" >
+                                </div>
+                                <div class="PType" style="margin-left: 5%;">
+                                    <input onchange="onTypeChoice(event)" onload="onTypeChoice(event)" id="typeList" class="small" name="ProductType" list="test" placeholder="Choose the type of the selling" required>
+                                    <datalist id="test">
+                                        <option value="Auction">
+                                        <option value="Best Offer">
+                                        <option value="Instant Buy">
+                                    </datalist>
+                                </div>
+                                <div class="PDescription"><input class="large" name="ProductDescription" type="textarea" placeholder="Product Description" style="height: 200px;" required></div>
+                                <div class="PPrice">
+                                    <div class="Price"><input class="small" name="ProductPrice" type="number" placeholder="Product Price ($)" required></div>
+                                </div>
+                                <div class="PEd" id="dateEnd"><a style="margin-left: 33%;">Select a DeadLine</a> <input class="small" name="ProductDeadLine" type="date" id="dateEndInput"></div>
+                                <div class="PPhoto">
+                                    <div class="Photo">
+                                        <a style="margin-left: 33%;">Add some pictures ?</a>
+                                        <input class="small" placeholder="http://google.com/image.png" name="ProductImage" type="url" required>
+                                    </div>
+                                </div>
+                                <!-- <div class="Video" >
                                         <a style="margin-left: 33%;">Or videos ?</a>
                                         <input class="small" name="ProductVideo" type="file" ></div>
                                     </div> -->
-                                 <hr>
-                                 <div style="margin-left: 45%;">
+                                <hr>
+                                <div style="margin-left: 45%;">
                                     <button type="submit">Post the Product</button>
                                 </div>
-                                 <hr>
+                                <hr>
                             </form>
                         </div>
                     </div>
