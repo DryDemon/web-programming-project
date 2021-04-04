486870ea4b8032e2eea328176cd6e19a8799b02b
jean chartier-kastler <jean.chartierkastler@gmail.com>
2021-04-04 15:33:27 +0200

    creation template pour user history

---
+++ template/userProfileHistory.php
@@ -0,0 +1,3 @@
+<li><?php echo $name ?><strong><?php if ($buying) echo 'SOLD';
+                                else echo "BOUGHT"; ?> $<?php echo $price ?></strong></li>
+                                
